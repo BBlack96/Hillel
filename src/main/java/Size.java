2294fb3cbc41b273fb7b1d82c3c13612a588aae6
BBlack96 <bogdan7450@gmail.com>
2022-06-26 11:28:13 +0300

    -DZ Enum

---
+++ src/main/java/Size.java
@@ -0,0 +1,29 @@
+public enum Size {
+    SMALL("S", 11, 40), MEDIUM("M", 30, 70),
+    LARGE("L", 50, 100),  EXTRA_LARGE("XL", 100, 150);
+    private String name;
+    private int width, length;
+
+
+    Size(String name, int width, int length) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public String toString(){
+        return name+" size: "+"width = "+ width+", "+"length = "+ length;
+    }
+}
+
