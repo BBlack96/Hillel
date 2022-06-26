0684863cd969d7d9a6deb05794266ef355e4d5e2
BBlack96 <bogdan7450@gmail.com>
2022-06-26 13:59:22 +0300

    -DZ Enum edits

--- src/main/java/Size.java
+++ src/main/java/Size.java
@@ -1,6 +1,6 @@
 public enum Size {
-    SMALL("S", 11, 40), MEDIUM("M", 30, 70),
-    LARGE("L", 50, 100),  EXTRA_LARGE("XL", 100, 150);
+    SMALL( "S or SMALL" , 11, 40), MEDIUM("M or MEDIUM", 30, 70),
+    LARGE("L or LARGE", 50, 100),  EXTRA_LARGE("XL or EXTRA LARGE", 100, 150);
     private String name;
     private int width, length;
 
